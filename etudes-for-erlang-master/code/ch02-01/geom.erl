7f7108fd5ca57b443587b1c6ec21c22efad21a47
crupib <crupib@gmail.com>
2019-09-12 10:19:47 -0400

    sync erlang

--- etudes-for-erlang-master/code/ch02-01/geom.erl
+++
@@ -1,5 +0,0 @@
--module(geom).
--export([area/2]).
-
-area(L,W) -> L * W.
-
--- etudes-for-erlang-master/code/ch10-02/pet_records.hrl
+++
@@ -1,5 +0,0 @@
--record(person,
-  {id_number, name, age, gender, city, amount_owed}).
--record(animal,
-  {id_number, name, species, gender, owner_id}).
-
