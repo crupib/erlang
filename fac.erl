5e1b188c27d865d3c9ab39387da686e271158729
crupib <crupib@gmail.com>
2020-01-30 21:07:29 -0500

    fac updates

--- fac.erl
+++ fac.erl
@@ -1,14 +1,9 @@
-%% ---
-%%  Excerpted from "Programming Erlang, Second Edition",
-%%  published by The Pragmatic Bookshelf.
-%%  Copyrights apply to this code. It may not be used to create training material, 
-%%  courses, books, articles, and the like. Contact us if you are in doubt.
-%%  We make no guarantees that this code is fit for any purpose. 
-%%  Visit http://www.pragmaticprogrammer.com/titles/jaerlang2 for more book information.
-%%---
 -module(fac).  
--export([fac/1]). 
+-export([fac/1,start/0]). 
 
 fac(0) -> 1;      
 fac(N) -> N*fac(N-1).
 
+start() -> 
+   X = fac(4), 
+   io:fwrite("~w~n",[X]).
