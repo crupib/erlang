57cd377f8e930a01bc845e798ade031df9c34742
crupib <crupib@gmail.com>
2021-08-29 21:23:27 -0400

    io

--- ask.erl
+++ ask.erl
@@ -3,5 +3,16 @@
 
 term() ->
   Input = io:read("What {planemo, distance} ? >>"),
-  Term = element(2, Input),
-  drop_tuples:fall_velocity(Term).
+  process_term(Input).
+
+process_term({ok, Term}) when is_tuple(Term) ->
+ Velocity = drop_tuples:fall_velocity(Term),
+ io:format("Yields ~w. ~n",[Velocity]),
+ term();
+
+process_term({ok,quit}) -> io:format("Goodbye.~n");
+
+process_term({ok,_}) -> io:format("You must enter a tuple.~n"),
+  term();
+
+process_term({error,_}) -> io:format("You must enter a tuple with correct syntax.~n").
