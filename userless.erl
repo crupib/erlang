f63cb49c91c14a7ee0e2ec33e6360dcbb91b734e
root <crupib@gmail.com>
2023-03-28 13:45:13 -0400

    erlang updates

---
+++ userless.erl
@@ -0,0 +1,13 @@
+-module(userless).
+-export([add/2,hello/0, greet_and_add_two/1]).
+-define(HOUR, 3600). % in seconds
+-define(sub(X,Y),X-Y).
+add(A,B) ->
+    A + B.
+hello() ->
+   io:format("Hello, world!~n").
+greet_and_add_two(X) ->
+  hello(),
+  add(X,2),
+  io:fwrite("~w",[?sub(5,3)]),
+  io:format("~n~n").
